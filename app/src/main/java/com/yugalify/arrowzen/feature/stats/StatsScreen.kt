15f1420688ec6d1c6bd5eaea2fca13c9f6db5c2c
Yugal <74865228+yugalofficial@users.noreply.github.com>
2026-09-12 10:04:48 +0530

    Fix: Import RoundedCornerShape from correct package

--- app/src/main/java/com/yugalify/arrowzen/feature/stats/StatsScreen.kt
+++ app/src/main/java/com/yugalify/arrowzen/feature/stats/StatsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RoundedCornerShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.yugalify.arrowzen.ArrowZenApplication
